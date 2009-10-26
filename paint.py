140289476760b43c70266451b15332bd424b1211
Mike Koss <mckoss@startpad.org>
2009-10-26 12:23:23 -0700

    iterator working with tests

--- paint.py
+++ paint.py
@@ -37,6 +37,44 @@ def usage():
 def solve_problem(problem):
     return None
 
+class paint_iter(object):
+    """ Iterator for paint by numbers runs.  Returns all combinations
+    of starting positions for each run """
+    def __init__(self, n, aRuns):
+        self.n = n
+        self.aRuns = aRuns
+        self.size = len(self.aRuns)
+        
+        pos = 0
+        self.aPos = []
+        for r in aRuns:
+            self.aPos.append(pos)
+            pos += r + 1
+            
+        if pos > self.n:
+            raise Exception("Runs %r won't fit in allotted space of %d squares." % (aRuns, n))
+
+        self.fFirst = True
+        
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.fFirst:
+            self.fFirst = False
+            return self.aPos
+        i = self.size - 1
+        while True:
+            if i < 0:
+                raise StopIteration
+            self.aPos[i] += 1
+            for j in range(i+1, self.size):
+                self.aPos[j] = self.aPos[j-1] + self.aRuns[j-1] + 1
+            if self.aPos[self.size-1] + self.aRuns[self.size-1] > self.n:
+                i -= 1
+                continue
+            return self.aPos
+
 # --------------------------------------------------------------------
 # Unit Tests
 # --------------------------------------------------------------------
@@ -47,6 +85,20 @@ import test_data
 class TestPBN(unittest.TestCase):
     def test_Basic(self):
         self.assertEqual(2, 2)
+        
+    def test_iter(self):
+        pi = [list(i) for i in paint_iter(10, [1])]
+        self.assertEqual(pi[0], [0])
+        self.assertEqual(len(pi), 10)
+        
+        pi = [list(i) for i in paint_iter(10, [2])]
+        self.assertEqual(len(pi), 9)
+        
+        pi = [list(i) for i in paint_iter(4, [1,1])]
+        self.assertEqual(pi, [[0,2], [0,3], [1, 3]])
+        
+    def test_iter_error(self):
+        self.assertRaises(Exception, lambda x: paint_iter(3, [2,2]))
 
 if __name__ == "__main__":
     main()
